295d413aafa6c4ef247efd4f632c0f0ea5e4b630
VoyDcode <Victor_r.lima@hotmail.com>
2026-04-12 02:17:56 -0300

    fix: corrige authUrl de produção para /api/auth

--- projects/nutryon-app/src/environments/environment.prod.ts
+++ projects/nutryon-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://nutryon-f8h2e8bqa0d7gjbx.southafricanorth-01.azurewebsites.net/api',
-  authUrl: 'https://nutryon-f8h2e8bqa0d7gjbx.southafricanorth-01.azurewebsites.net/auth'
+  authUrl: 'https://nutryon-f8h2e8bqa0d7gjbx.southafricanorth-01.azurewebsites.net/api/auth'
 };
